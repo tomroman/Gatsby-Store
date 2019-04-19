7401fed99c6e85f7044a8859764ecb1ea7cde3d8
Thomas Roman <thomasroman32@yahoo.com>
2019-04-18 22:45:11 -0500

    fixed component miss spelling error in index.js. Also deleted imports that were causing errors. Hot-Reloading working again. Need to set up account or business name in stripe.

---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/thomasroman/Desktop/Gatsby-Repositories/Gatsby-Store/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/thomasroman/Desktop/Gatsby-Repositories/Gatsby-Store/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/thomasroman/Desktop/Gatsby-Repositories/Gatsby-Store/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/Users/thomasroman/Desktop/Gatsby-Repositories/Gatsby-Store/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/thomasroman/Desktop/Gatsby-Repositories/Gatsby-Store/.cache/data.json")
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 import SEO from "../components/seo"
 
-class Button extends React.Componenet {
+class Button extends React.Component {
+
   componentDidMount() {
-    this.stripe = Stripe('pk_test_K9AD7LxDvcFm2QCFUp6CWozG00IkLvxGoz', {betas: ['checkout_beta_4']});
+    this.stripe = window.Stripe('pk_test_K9AD7LxDvcFm2QCFUp6CWozG00IkLvxGoz', {
+      betas: ['checkout_beta_4'],
+    })
   }
 
   render() {
     return (
       <form onSubmit={event => {
+        console.log("Hello")
         event.preventDefault();
         this.stripe.redirectToCheckout({
-          items: [{sku: 'sku_Eubk4P0rcAf2U6', quantity: 1}],
-    
+          items: [{ sku: 'sku_Eubk4P0rcAf2U6', quantity: 1 }],
+
           // Note that it is not guaranteed your customers will be redirected to this
           // URL *100%* of the time, it's possible that they could e.g. close the
           // tab between form submission and the redirect.
           successUrl: 'https://localhost:8000/success',
           cancelUrl: 'https://localhost:8000/canceled',
         })
-        .then(function (result) {
-          if (result.error) {
-            // If `redirectToCheckout` fails due to a browser or network
-            // error, display the localized error message to your customer.
-            var displayError = document.getElementById('error-message');
-            displayError.textContent = result.error.message;
-          }
-        });
+          .then(function (result) {
+            if (result.error) {
+              // If `redirectToCheckout` fails due to a browser or network
+              // error, display the localized error message to your customer.
+              var displayError = document.getElementById('error-message');
+              displayError.textContent = result.error.message;
+            }
+          });
       }}>
+        <button type="submit">Buy Pants</button>
       </form>
     )
   }
 }
-  
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Buy Freelance Pants</h1>
-    <button> Buy Pants</button>
-   
+    <Button />
+
   </Layout>
 )
 
